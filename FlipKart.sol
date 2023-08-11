3dead57c03dd4d107b96dad8904568d10dd92fff
likith-02 <76258283+likith-02@users.noreply.github.com>
2023-08-12 02:05:16 +0530

    Update FlipKart.sol

--- FlipKart.sol
+++ FlipKart.sol
@@ -1,1 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import "./Token.sol";
+
+contract FlipKart {
+    address private token;
+    constructor(){
+        token = address(new FlipToken());
+    }
+}
