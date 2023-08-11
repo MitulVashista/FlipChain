357be3b9dbf01924e5ec313a25a8efdb0655483a
likith-02 <76258283+likith-02@users.noreply.github.com>
2023-08-12 02:00:11 +0530

    Update flipcoin.sol

--- flipcoin.sol
+++ flipcoin.sol
@@ -1,60 +1,28 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
-
-contract FlipCoinERC20 {
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-
-    string public constant name = "FlipCoin";
-    string public constant symbol = "FCN";
-    uint8 public constant decimals = 18;
-
-    mapping(address => uint256) balances;
-
-    mapping(address => mapping (address => uint256)) allowed;
-
-    uint256 totalSupply_;
-
-    constructor(uint256 total) {
-      totalSupply_ = total;
-      balances[msg.sender] = totalSupply_;
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+contract FlipToken is ERC20("flipCoin", "FP"),Ownable(msg.sender){
+    function mintFifty() public onlyOwner {
+        _mint(msg.sender, 50 * 10**18);
     }
 
-    function totalSupply() public view returns (uint256) {
-      return totalSupply_;
-    }
-
-    function balanceOf(address tokenOwner) public view returns (uint) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        balances[msg.sender] -= numTokens;
-        balances[receiver] += numTokens;
-        emit Transfer(msg.sender, receiver, numTokens);
+    function transfer(address to, uint256 value) public onlyOwner virtual override  returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
         return true;
     }
 
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
+    function approve(address owner, uint256 value) public onlyOwner virtual override  returns (bool) {
+        address spender = _msgSender();
+        _approve(owner, spender, value);
         return true;
     }
 
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate];
-    }
-
-    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-
-        balances[owner] -= numTokens;
-        allowed[owner][msg.sender] -= numTokens;
-        balances[buyer] += numTokens;
-        emit Transfer(owner, buyer, numTokens);
+    function transferFrom(address from, address to, uint256 value) public onlyOwner virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
         return true;
     }
 }
