b2301c64200f25e536cb100d8251203734291867
MitulVashista <vashista.1@iitj.ac.in>
2023-08-13 20:43:07 +0530

    modified:   platform.sol 	modified:   token.sol

--- token.sol
+++ token.sol
@@ -4,27 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FlipToken is ERC20("flipCoin", "FP"), Ownable{
-    function mintFifty() public onlyOwner {
-        _mint(msg.sender, 50 * 10**18);
+contract FlipToken is ERC20("FlipCoin", "FPC"), Ownable{
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
     }
 
-    function transfer(address to, uint256 value) public onlyOwner virtual override  returns (bool) {
-        address owner = _msgSender();
-        _transfer(owner, to, value);
-        return true;
-    }
-
-    function approve(address owner, uint256 value) public onlyOwner virtual override  returns (bool) {
-        address spender = _msgSender();
-        _approve(owner, spender, value);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 value) public onlyOwner virtual override returns (bool) {
-        address spender = _msgSender();
-        _spendAllowance(from, spender, value);
-        _transfer(from, to, value);
-        return true;
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
     }
 }
