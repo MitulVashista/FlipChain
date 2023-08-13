35b6c5c8d251767a2c951b1ad80d0cbea9820817
likith-02 <76258283+likith-02@users.noreply.github.com>
2023-08-13 22:25:48 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,13 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract FlipToken is ERC20("FlipCoin", "FPC"), Ownable{
-
-    function mint(uint256 amount) public onlyOwner {
-        _mint(msg.sender, amount);
+pragma solidity 0.8.20;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+contract FlipToken is ERC20("flipCoin", "FP"),Ownable(msg.sender){
+    function mint(uint supply) public onlyOwner {
+        _mint(msg.sender, supply * 10**18);
     }
 
     function transfer(address to, uint256 value) public onlyOwner virtual override  returns (bool) {
